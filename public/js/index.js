0d050f33a50a91a49d3f2230cb08fca6af78edfd
Manuel <manuelsapology@gmail.com>
2018-12-31 16:25:50 -0400

    Emit newM on createM

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect',function() {
 	console.log('Connected to server');
 
-	socket.emit('createMessage', {
-		from:'jen',
-		text:'Hey nigga, what s poppin'
-	});
 });
 
 socket.on('disconnect',function() {
